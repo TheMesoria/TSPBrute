bdbac78525f0f595a5278af931b7f5aa65041825
Black <freedomandjustit@gmail.com>
2018-12-02 18:13:39 +0100

    Add cities

--- main.cpp
+++ main.cpp
@@ -10,25 +10,184 @@
 
 int main()
 {
-	auto sink   = std::make_shared<spdlog::sinks::simple_file_sink_mt>( CMAKE_CURRENT_SOURCE_DIR + "/logs/run.log" );
-	auto logger = std::make_shared<spdlog::logger>( "main", sink );
-	spdlog::register_logger( logger );
+    auto sink   = std::make_shared<spdlog::sinks::simple_file_sink_mt>(
+        CMAKE_CURRENT_SOURCE_DIR + "/logs/run.log");
+    auto logger = std::make_shared<spdlog::logger>("main", sink);
+    spdlog::register_logger(logger);
 
-	spdlog::get( "main" )->set_level( spdlog::level::info );
-	spdlog::get( "main" )->flush_on( spdlog::level::info );
-	spdlog::get( "main" )->info( "Initialisation done." );
+    spdlog::get("main")->set_level(spdlog::level::info);
+    spdlog::get("main")->flush_on(spdlog::level::info);
+    spdlog::get("main")->info("Initialisation done.");
 
-	Map map( "/burma14.city" );
+    Map map_4("/own4.city");
+    Map map_5("/own5.city");
+    Map map_6("/own6.city");
+    Map map_7("/own7.city");
+    Map map_8("/own8.city");
+    Map map_9("/own9.city");
+    Map map_10("/own10.city");
 
-	std::unique_ptr<Algorithm> sbf   = std::make_unique<StandardBruteForce>( map );
-	std::unique_ptr<Algorithm> omtbf = std::make_unique<OptimalMTBruteForce>( map );
-	std::unique_ptr<Algorithm> babmtbf = std::make_unique<BranchAndBoundMTBruteForce>( map );
 
-//	sbf->start();
-	omtbf->start();
-	babmtbf->start();
+    auto start = std::chrono::steady_clock::now();
 
-	spdlog::get( "main" )->info( "Run Finished!" );
+    std::unique_ptr<Algorithm> sbf = std::make_unique<StandardBruteForce>(map_4);
+    sbf->start();
 
-	return EXIT_SUCCESS;
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
+        std::chrono::steady_clock::now() - start);
+    spdlog::get("main")->info("Duration : {} microseconds", duration.count());
+    std::cerr << "RESULT : " << duration.count() << std::endl;
+
+    start = std::chrono::steady_clock::now();
+
+    sbf = std::make_unique<StandardBruteForce>(map_5);
+    sbf->start();
+
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(
+        std::chrono::steady_clock::now() - start);
+    spdlog::get("main")->info("Duration : {} microseconds", duration.count());
+    std::cerr << "RESULT : " << duration.count() << std::endl;
+
+    start = std::chrono::steady_clock::now();
+
+    sbf = std::make_unique<StandardBruteForce>(map_6);
+    sbf->start();
+
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(
+        std::chrono::steady_clock::now() - start);
+    spdlog::get("main")->info("Duration : {} microseconds", duration.count());
+    std::cerr << "RESULT : " << duration.count() << std::endl;
+
+    start = std::chrono::steady_clock::now();
+
+    sbf = std::make_unique<StandardBruteForce>(map_7);
+    sbf->start();
+
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(
+        std::chrono::steady_clock::now() - start);
+    spdlog::get("main")->info("Duration : {} microseconds", duration.count());
+    std::cerr << "RESULT : " << duration.count() << std::endl;
+
+    start = std::chrono::steady_clock::now();
+
+    sbf = std::make_unique<StandardBruteForce>(map_8);
+    sbf->start();
+
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(
+        std::chrono::steady_clock::now() - start);
+    spdlog::get("main")->info("Duration : {} microseconds", duration.count());
+    std::cerr << "RESULT : " << duration.count() << std::endl;
+
+    start = std::chrono::steady_clock::now();
+
+    sbf = std::make_unique<StandardBruteForce>(map_9);
+    sbf->start();
+
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(
+        std::chrono::steady_clock::now() - start);
+    spdlog::get("main")->info("Duration : {} microseconds", duration.count());
+    std::cerr << "RESULT : " << duration.count() << std::endl;
+
+    start = std::chrono::steady_clock::now();
+
+    sbf = std::make_unique<StandardBruteForce>(map_10);
+    sbf->start();
+
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(
+        std::chrono::steady_clock::now() - start);
+    spdlog::get("main")->info("Duration : {} microseconds", duration.count());
+    std::cerr << "RESULT : " << duration.count() << std::endl;
+
+
+
+    spdlog::get("main")->info("");
+    spdlog::get("main")->info("NEW");
+    spdlog::get("main")->info("");
+    std::cerr << "RESULT : " << std::endl;
+    std::cerr << "RESULT : " << std::endl;
+    std::cerr << "RESULT : " << std::endl;
+
+
+
+    start = std::chrono::steady_clock::now();
+
+    std::unique_ptr<Algorithm> babmtbf = std::make_unique<OptimalMTBruteForce>(map_4);
+    babmtbf->start();
+
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(
+        std::chrono::steady_clock::now() - start);
+    spdlog::get("main")->info("Duration : {} microseconds", duration.count());
+    std::cerr << "RESULT : " << duration.count() << std::endl;
+
+    start = std::chrono::steady_clock::now();
+
+    babmtbf = std::make_unique<StandardBruteForce>(map_5);
+    babmtbf->start();
+
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(
+        std::chrono::steady_clock::now() - start);
+    spdlog::get("main")->info("Duration : {} microseconds", duration.count());
+    std::cerr << "RESULT : " << duration.count() << std::endl;
+
+    start = std::chrono::steady_clock::now();
+
+    babmtbf = std::make_unique<StandardBruteForce>(map_6);
+    babmtbf->start();
+
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(
+        std::chrono::steady_clock::now() - start);
+    spdlog::get("main")->info("Duration : {} microseconds", duration.count());
+    std::cerr << "RESULT : " << duration.count() << std::endl;
+
+    start = std::chrono::steady_clock::now();
+
+    babmtbf = std::make_unique<StandardBruteForce>(map_7);
+    babmtbf->start();
+
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(
+        std::chrono::steady_clock::now() - start);
+    spdlog::get("main")->info("Duration : {} microseconds", duration.count());
+    std::cerr << "RESULT : " << duration.count() << std::endl;
+
+    start = std::chrono::steady_clock::now();
+
+    babmtbf = std::make_unique<StandardBruteForce>(map_8);
+    babmtbf->start();
+
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(
+        std::chrono::steady_clock::now() - start);
+    spdlog::get("main")->info("Duration : {} microseconds", duration.count());
+    std::cerr << "RESULT : " << duration.count() << std::endl;
+
+    start = std::chrono::steady_clock::now();
+
+    babmtbf = std::make_unique<StandardBruteForce>(map_9);
+    babmtbf->start();
+
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(
+        std::chrono::steady_clock::now() - start);
+    spdlog::get("main")->info("Duration : {} microseconds", duration.count());
+    std::cerr << "RESULT : " << duration.count() << std::endl;
+
+    start = std::chrono::steady_clock::now();
+
+    babmtbf = std::make_unique<StandardBruteForce>(map_10);
+    babmtbf->start();
+
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(
+        std::chrono::steady_clock::now() - start);
+    spdlog::get("main")->info("Duration : {} microseconds", duration.count());
+    std::cerr << "RESULT : " << duration.count() << std::endl;
+
+    // std::unique_ptr<Algorithm> sbf   = std::make_unique<StandardBruteForce>( map );
+    // std::unique_ptr<Algorithm> omtbf = std::make_unique<OptimalMTBruteForce>( map );
+    // std::unique_ptr<Algorithm> babmtbf = std::make_unique<BranchAndBoundMTBruteForce>( map );
+
+    //	sbf->start();
+    // 	omtbf->start();
+    // 	babmtbf->start();
+
+    spdlog::get("main")->info("Run Finished!");
+
+    return EXIT_SUCCESS;
 }
